921a5e6440aff21d4b348f841bf6d89e64598799
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-06-13 17:38:35 -0400

    updated handleSubmit, will continue working on hydration errors

--- components/Contact.js
+++ components/Contact.js
@@ -1,36 +1,49 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import userData from "@constants/data";
+import Link from "next/link";
+import { sendContactForm } from "@lib/api";
+
+const initValues = {
+  name: "",
+  email: "",
+  message: "",
+};
+
+const initState = { values: initValues };
 
 export default function Contact() {
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const [state, setState] = useState(initState);
+  const [hasMounted, setHasMounted] = useState(false);
 
-    const data = {
-      name: event.target.name.value,
-      email: event.target.email.value,
-      message: event.target.message.value,
-    };
+  // const [form, setForm] = useState("");
+  const { values, isLoading } = state;
 
-    console.log(data.email);
-
-    const response = await fetch("/api/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+  const handleChange = ({ target }) =>
+    setState((prev) => ({
+      ...prev,
+      values: {
+        ...prev.values,
+        [target.name]: target.value,
       },
-      body: JSON.stringify(data),
-    });
+    }));
 
-    if (response.ok) {
-      console.log("Message sent successfully");
-    }
-
-    if (!response.ok) {
-      console.log("Error sending message");
-    }
+  const onSubmit = async () => {
+    setState((prev) => ({
+      ...prev,
+      isLoading: true,
+    }));
+    await sendContactForm(values);
   };
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
 
   return (
     <section>
@@ -101,7 +114,8 @@ export default function Contact() {
               </div>
             </div>
             <div className="social-icons flex flex-row space-x-8">
-              <a
+              <Link
+                passHref
                 href={userData.socialLinks.facebook}
                 className="h-10 w-10 rounded-full hover:bg-blue-500 flex items-center justify-center cursor-pointer"
               >
@@ -118,8 +132,9 @@ export default function Contact() {
                     fill="currentColor"
                   />
                 </svg>
-              </a>
-              <a
+              </Link>
+              <Link
+                passHref
                 href={userData.socialLinks.twitter}
                 className="h-10 w-10 rounded-full hover:bg-blue-500 flex items-center justify-center cursor-pointer"
               >
@@ -138,8 +153,9 @@ export default function Contact() {
                     fill="currentColor"
                   />
                 </svg>
-              </a>
-              <a
+              </Link>
+              <Link
+                passHref
                 href={userData.socialLinks.instagram}
                 className="h-10 w-10 rounded-full hover:bg-blue-500 flex items-center justify-center cursor-pointer"
               >
@@ -168,29 +184,36 @@ export default function Contact() {
                     fill="currentColor"
                   />
                 </svg>
-              </a>
+              </Link>
             </div>
           </div>
           <form
-            onSubmit={handleSubmit}
+            onSubmit={onSubmit}
             className="form rounded-lg bg-white p-4 flex flex-col"
+            isInvalid={true}
           >
             <label htmlFor="name" className="text-sm text-gray-600 mx-4">
               {" "}
               Your Name
             </label>
             <input
+              required
               type="text"
               className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
               name="name"
+              value={values.name}
+              onChange={handleChange}
             />
             <label htmlFor="email" className="text-sm text-gray-600 mx-4 mt-4">
               Email
             </label>
             <input
-              type="text"
+              required
+              type="email"
               className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
               name="email"
+              value={values.email}
+              onChange={handleChange}
             />
             <label
               htmlFor="message"
@@ -199,10 +222,13 @@ export default function Contact() {
               Message
             </label>
             <textarea
+              required
               rows="4"
               type="text"
               className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
               name="message"
+              value={values.message}
+              onChange={handleChange}
             ></textarea>
             <button
               type="submit"
