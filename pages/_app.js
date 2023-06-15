2daa1992fe4acb346984ef6d1cace9021244156c
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-06-15 16:55:51 -0400

    updated all svgs to components, still getting hydration error, completed form functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
       <Component {...pageProps} />
+      <ToastContainer />
     </ThemeProvider>
   );
 }
